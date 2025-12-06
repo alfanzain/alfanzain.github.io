518f4104c7e6fb7a903d4844a9250dbed47e37a1
Alfan Zain Putra <alfanzainkp@gmail.com>
2025-12-06 18:52:51 +0700

    Convert into static website

--- README.md
+++
@@ -1,1 +0,0 @@
-alfanzain.github.io
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',      // kalau kamu cuma pakai HTML ini
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
